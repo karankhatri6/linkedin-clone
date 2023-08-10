3c1c42f2918020870a51db53b1e03ea8211d599b
Karan Khatri <karankhatri535@gmail.com>
2023-08-10 23:43:30 +0530

    home page navbar's logo and search bar done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
+import { Fragment } from 'react';
 
 function App() {
   return (
@@ -8,6 +11,13 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<Login />}></Route>
+          <Route path='home' element={
+            <Fragment>
+              <Header />
+              <Home />
+            </Fragment>
+          }>
+          </Route>
         </Routes>
       </Router>
     </div>
