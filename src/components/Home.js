9305cdbdabe76b0ce519dd6a710e04f59f6ef75c
Karan Khatri <karankhatri535@gmail.com>
2023-08-20 23:46:25 +0530

    created left side card of the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled  from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = (props) => {
     return (
@@ -11,6 +14,11 @@ const Home = (props) => {
                     Find talented pros in record time with Upwork and keep business moving.
                 </p>
             </Section>
+            <Layout>
+                <Leftside />
+                <Main />
+                <Rightside />
+            </Layout>
         </Container>
     )
 }
@@ -49,6 +57,21 @@ const Section = styled.div`
   }
 
   @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
     flex-direction: column;
     padding: 0 5px;
   }
