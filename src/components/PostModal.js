07ee3eea7d54fbdbee68509554c8f541dad01f90
Karan Khatri <karankhatri535@gmail.com>
2023-10-10 16:42:48 +0530

    upload images and videos to firebase storage

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import styled  from "styled-components";
 import ReactPlayer from "react-player";
-import { Connect, connect } from "react-redux";
+import { connect } from "react-redux";
+import firebase from 'firebase/compat/app';
+import { postArticleAPI } from "../actions";
 
 const PostModal = (props) => {
     const [ editorText, setEditorText ] = useState("")
@@ -24,6 +26,24 @@ const PostModal = (props) => {
       setShareImage("")
       setVideoLink("")
       setAssetArea(area)
+    }
+
+    const postArticle = (e) => {
+      e.preventDefault();
+      if (e.target !== e.currentTarget){
+        return;
+      }
+
+      const payload = {
+        image: shareImage,
+        video: videoLink,
+        user: props.user,
+        description: editorText,
+        timestamp: firebase.firestore.Timestamp.now()
+      };
+
+      props.postArticle(payload)
+      reset(e)
     }
 
     const reset = (e) => {
@@ -105,7 +125,12 @@ const PostModal = (props) => {
                         Anyone
                       </AssetButton>
                     </ShareComment>
-                    <PostButton disabled={!editorText ? true : false}>Post</PostButton>
+                    <PostButton 
+                      disabled={!editorText ? true : false}
+                      onClick={(event) => postArticle(event)}
+                      >
+                        Post
+                      </PostButton>
                 </ShareCreation>
             </Content>
         </Container>
@@ -277,6 +302,8 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({})
+const mapDispatchToProps = (dispatch) => ({
+  postArticle: (payload) => dispatch(postArticleAPI(payload))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostModal)
